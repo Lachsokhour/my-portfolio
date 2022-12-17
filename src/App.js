44af35d24f5d86746756b5c06c11c585ac86f0b6
Lach Sokhour <60126739+Lachsokhour@users.noreply.github.com>
2022-12-17 15:11:19 +0700

    Initial Project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="text-3xl text-center text-red-500 font-bold">
+      To be continue
+    </h1>
+  );
+}
+
+export default App;
